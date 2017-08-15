d90c446331b6d22ebc977cc9d21292c73d48ff9e
Simone Grignola <sito@grignola.ch>
2017-08-15 23:32:16 +0200

    Removed hard-coded  parameter

--- NServiceBus.Transport.Email.Demo.Sender/Program.cs
+++ NServiceBus.Transport.Email.Demo.Sender/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Threading.Tasks;
 using NServiceBus.Features;
 using NServiceBus.Transport.Email.Demo.Shared;
@@ -17,17 +18,17 @@ namespace NServiceBus.Transport.Email.Demo.Sender
             Console.Title = "NServiceBus.Transport.Email.Demo.Sender";
             var endpointConfiguration = new EndpointConfiguration("SenderEndpointName");
             endpointConfiguration.UseTransport<EmailTransport>();
-            
+
             endpointConfiguration.UsePersistence<InMemoryPersistence>();
             endpointConfiguration.DisableFeature<TimeoutManager>();
 
             var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
 
-            var receiverName = "ReceiverEndpointName@sito@grignola.ch";
             var messageA = new MessageA();
-            await endpointInstance.Send(receiverName, messageA).ConfigureAwait(false);
+            var receiverEndpointName = ConfigurationManager.AppSettings["ReceiverEndpointName"];
+            await endpointInstance.Send(receiverEndpointName, messageA).ConfigureAwait(false);
 
-            Console.WriteLine("MessageA sent to endpoint {0}. Press any key to exit", receiverName);
+            Console.WriteLine("MessageA sent to endpoint {0}. Press any key to exit", receiverEndpointName);
             Console.ReadKey();
             await endpointInstance.Stop().ConfigureAwait(false);
         }
